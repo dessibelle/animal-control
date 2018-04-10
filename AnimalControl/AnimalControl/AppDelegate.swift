477230e2974113348d28d51f204411e64fa170d5
Simon Fransson <simon@funnel.io>
2018-04-10 13:25:36 +0200

    Added MenuBarItem app

---
+++ AnimalControl/AnimalControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AnimalControl
+//
+//  Created by Simon Fransson on 2018-04-10.
+//  Copyright © 2018 Simon Fransson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
